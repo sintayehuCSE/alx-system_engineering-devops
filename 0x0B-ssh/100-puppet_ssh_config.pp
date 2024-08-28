e7c5acaa94aaefe14b39ffcfa72c9d56176ec341
Sintayehu Mulugeta Kebede <sintayehumulugeta77@gmail.com>
2024-08-28 17:55:44 +0300

    Puupet Client Side Configuration

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+# Configure client-side ssh configuration file
+
+file_line { 'Turn off password auth':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication = no'
+}
+
+file_line { 'Add Identity Key':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile = ~/.ssh/school'
+}
